f9d19c83d861102f141b1bf911d1fdbc8f0101ae
manish52kar <manish52kar@gmail.com>
2019-03-22 22:59:08 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/")
+	public String wel() {
+		return "welcome to J";
+	}
+	
+	@GetMapping("/{id}")
+	public String wel2(@PathVariable String id) {
+		return id;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
